50270abe5432f717b7925389704870c8182c07e9
Sougata Karmakar <sougatakarmakar151@gmail.com>
2026-09-15 00:08:17 +0530

    Add API key to dark map TileLayer URL

--- src/pages/EngineerPortal.jsx
+++ src/pages/EngineerPortal.jsx
@@ -610,7 +610,7 @@ ${htmlBody}
               zoomControl={false}
             >
               {mapStyle === 'dark' ? (
-                <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
+                <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?api_key=${import.meta.env.VITE_CARTO_API_KEY}" />
               ) : (
                 <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
               )}
